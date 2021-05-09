9413462a1d74c5728077b95bbb2fcce12b5383d6
Dknow <dknow7758521@live.com>
2021-05-09 23:39:31 +0800

    2021-5-9

--- HC/__init__.py
+++ HC/__init__.py
@@ -3,16 +3,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from config import *
-from Views import Users, Scan
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///healthcheck.db'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hc.sqlite'
 db = SQLAlchemy(app, use_native_unicode='utf8',
                 session_options={'autoflush': True, 'autocommit': True, 'expire_on_commit': True})
 
+from Views import Users, Scan
+
 app.register_blueprint(Users.api, url_prefix='/')
-app.register_blueprint(Scan.api, url_prefix='/')
+app.register_blueprint(Scan.api2, url_prefix='/')
 
 
 @app.route('/')
